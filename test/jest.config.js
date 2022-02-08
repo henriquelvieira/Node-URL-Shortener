d56586a40aa7e3c47a1f8e0330c990ac9042889e
Henrique Lopes <henrique.l.vieira92@gmail.com>
2022-02-08 11:06:08 -0300

    Delete jest.config.js

--- test/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    clearMocks: true,
-    coverageProvider: "v8",
-    preset: "ts-jest",
-    testMatch: ["**/functional/*.spec.ts"],
-};
-//# sourceMappingURL=jest.config.js.map
